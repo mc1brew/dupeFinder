a2d954c1378b5b24703abe0d8407708a6ad733b4
k <1282707+mc1brew@users.noreply.github.com>
2019-09-08 17:05:11 -0600

    - Beginning to prime for async processing. - Read the first Kilobyte instead of the first Centibyte. - Still need to chunk the file up... will get there.

--- ByteTool.cs
+++ ByteTool.cs
@@ -1,14 +1,15 @@
 using System;
 using System.IO;
 using System.Security.Cryptography;
+using System.Threading.Tasks;
 
 namespace dupeFinder{
     public class ByteTool {
 
-        public static string GetHundredByteMd5Hash(string path)
+        public static string GetKilobyteMd5Hash(string path)
         {
             string returnMd5Hash;
-            byte[] firstHundredBytes = GetHundredByteArray(path, 0);
+            byte[] firstHundredBytes = GetKilobyteArray(path, 0);
 
             using(MD5 crypt = MD5.Create())
             {
@@ -19,17 +20,19 @@ namespace dupeFinder{
             return returnMd5Hash;
         }
 
-        public static bool CompareByteArray(string path_A, string path_B)
+        public async static Task<bool> CompareByteArray(string path_A, string path_B)
         {
             FileInfo fileInfo_A = new FileInfo(path_A);
             FileInfo fileInfo_B = new FileInfo(path_B);
 
-            if(fileInfo_A.Length == fileInfo_B.Length)
+            if(fileInfo_A.Length != 0 && fileInfo_A.Length == fileInfo_B.Length)
             {
                 using(FileStream fileStream_A = new FileStream(path_A, FileMode.Open))
                 using(FileStream fileStream_B = new FileStream(path_B, FileMode.Open))
                 {
-                    int byteCounter = 0;
+                    long byteCounter = 0;
+                    //TODO: This would be a good place to implement some sort of parralel procesing.
+                    //You could chunk the byte array and then kick off a thread for each array.
                     while(byteCounter < fileInfo_A.Length && fileStream_A.ReadByte() == fileStream_B.ReadByte())
                     {
                         byteCounter++;
@@ -42,7 +45,7 @@ namespace dupeFinder{
         }
 
 
-        private static byte[] GetHundredByteArray(string path, int offset)
+        private static byte[] GetKilobyteArray(string path, int offset)
         {
             byte[] returnArray = new byte[1024];
             using(FileStream fs = new FileStream(path,FileMode.Open))
